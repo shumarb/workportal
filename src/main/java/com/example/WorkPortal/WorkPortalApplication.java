9761c68de3a7109e60f90ac85c11293a79dcc0cc
S.Umar <shumarb@outlook.com>
2024-07-17 12:41:05 +0800

    Added dependencies for project created from Spring Initializr

---
+++ src/main/java/com/example/WorkPortal/WorkPortalApplication.java
@@ -0,0 +1,13 @@
+package com.example.WorkPortal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkPortalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/WorkPortal/WorkPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WorkPortal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkPortalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
