09a4d2e1db5e7af4c8db48dd59b77bc98d185f5c
S.Umar <shumarb@outlook.com>
2024-07-17 23:09:33 +0800

    Created service layer for UserProfile objects

--- src/main/java/com/example/WorkPortal/service/UserProfileService.java
+++ src/main/java/com/example/WorkPortal/service/UserProfileService.java
@@ -1,2 +1,21 @@
-package com.example.WorkPortal.service;public class UserProfileService {
+package com.example.WorkPortal.service;
+
+import com.example.WorkPortal.model.UserProfile;
+import com.example.WorkPortal.repository.UserProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserProfileService {
+
+    private final UserProfileRepository userProfileRepository;
+
+    @Autowired
+    public UserProfileService(UserProfileRepository userProfileRepository) {
+        this.userProfileRepository = userProfileRepository;
+    }
+
+    public void registerUserProfile(UserProfile userProfile) {
+        this.userProfileRepository.save(userProfile);
+    }
 }
