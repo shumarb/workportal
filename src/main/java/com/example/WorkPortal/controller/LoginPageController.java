5dc15721377a3aacbbaff1915ec3f772ec1d8801
S.Umar <shumarb@outlook.com>
2024-07-18 14:17:00 +0800

    Created controller and its tests for login page

--- src/main/java/com/example/WorkPortal/controller/LoginPageController.java
+++ src/main/java/com/example/WorkPortal/controller/LoginPageController.java
@@ -1,2 +1,30 @@
-package com.example.WorkPortal.controller;public class LoginPageController {
+/**
+ * Registration controller is a controller class that handles requests
+ * related to the login page.
+ */
+package com.example.WorkPortal.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginPageController {
+    /**
+     * Logger to monitor operational flow and assist in troubleshooting.
+     */
+    private static final Logger loginPageControllerLogger = LogManager.getLogger(IndexPageController.class);
+
+    /**
+     * Direction to the Signup page.
+     *
+     * @return name of the signup page.
+     */
+    @GetMapping("/login")
+    public String goToLoginPage() {
+        loginPageControllerLogger.info("LoginPageControllerLogger: User is now at login.html.");
+        return "login";
+    }
+
 }
--- src/test/java/com/example/WorkPortal/controller/LoginPageControllerTest.java
+++ src/test/java/com/example/WorkPortal/controller/LoginPageControllerTest.java
@@ -1,4 +1,33 @@
+package com.example.WorkPortal.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class LoginPageControllerTest {
-  
+
+    private String pageName;
+
+    @BeforeEach
+    public void setUp() {
+        LoginPageController loginPageController = new LoginPageController();
+        this.pageName = loginPageController.goToLoginPage();
+    }
+
+    @Test
+    void test_retrievesLoginPage() {
+        assertEquals("login", pageName);
+    }
+
+    @Test
+    void test_doesNotRetrieveIndexPage() {
+        assertNotEquals("index", pageName);
+    }
+
+    @Test
+    void test_doesNotRetrieveRegistrationPage() {
+        assertNotEquals("registration", pageName);
+    }
+
 }
