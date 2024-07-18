68aec4b3ef4b0d1ffa62fe8d8e76ff4704467735
S.Umar <shumarb@outlook.com>
2024-07-18 18:48:35 +0800

    Added methods to find Person by email and username

--- src/main/java/com/example/WorkPortal/repository/PersonRepository.java
+++ src/main/java/com/example/WorkPortal/repository/PersonRepository.java
@@ -7,6 +7,17 @@ package com.example.WorkPortal.repository;
 import com.example.WorkPortal.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    /**
+     * Retrieves a Person by email
+     */
+    Optional<Person> findByEmail(String email);
+
+    /**
+     * Retrieves a Person by username
+     */
+    Optional<Person> findByUsername(String username);
 }
