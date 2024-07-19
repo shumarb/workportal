0b3b33d66be6082bc33274013c129e5da98668d1
S.Umar <shumarb@outlook.com>
2024-07-19 22:25:47 +0800

    Updated InvalidUsernameOrPasswordException

--- src/main/java/com/example/WorkPortal/exceptions/InvalidUsernameOrPasswordException.java
+++ src/main/java/com/example/WorkPortal/exceptions/InvalidUsernameOrPasswordException.java
@@ -1,2 +1,9 @@
-package com.example.WorkPortal.exceptions;public class InvalidUsernameOrPasswordException {
+/**
+ * Exception thrown for invalid username or password encountered during login.
+ */
+
+package com.example.WorkPortal.exceptions;
+
+public class InvalidUsernameOrPasswordException extends Exception {
+
 }
