d20d4a76c9d2bd6e7c22c4781a66ace67467f590
S.Umar <shumarb@outlook.com>
2024-07-20 22:44:35 +0800

    Created logout button and implemented its functionality

--- src/main/java/com/example/WorkPortal/controller/HomeController.java
+++ src/main/java/com/example/WorkPortal/controller/HomeController.java
@@ -4,10 +4,14 @@
 
 package com.example.WorkPortal.controller;
 
+import com.example.WorkPortal.model.Person;
+import jakarta.servlet.http.HttpSession;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HomeController {
@@ -23,9 +27,26 @@ public class HomeController {
      * @return Name of the Home page.
      */
     @GetMapping("/home")
-    public String showHome() {
+    public String showHome(HttpSession httpSession, Model model) {
+        Person loggedInPerson = (Person) httpSession.getAttribute("loggedInPerson");
+        if (loggedInPerson == null) {
+            return "redirect:/login";
+        }
+        model.addAttribute("loggedInPerson", loggedInPerson);
         homeControllerLogger.info("HomeControllerLogger: Currently at Home page.");
         return "home";
+    }
+
+    /**
+     * Handles the GET request of logout button of Home page.
+     *
+     * @return Name of the Index page.
+     */
+    @PostMapping("/logout")
+    public String logoutOfHome(HttpSession session) {
+        homeControllerLogger.info("HomeControllerLogger: Logging out from Home page. Going to Index page.");
+        session.invalidate();
+        return "index";
     }
 
 }
