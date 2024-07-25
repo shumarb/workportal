3fd5039a14d69c0f72b0a833d29f7331c3395ccb
S.Umar <shumarb@outlook.com>
2024-07-25 22:43:14 +0800

    Added Repository annotation

--- src/main/java/com/example/WorkPortal/repository/PersonRepository.java
+++ src/main/java/com/example/WorkPortal/repository/PersonRepository.java
@@ -6,9 +6,11 @@ package com.example.WorkPortal.repository;
 
 import com.example.WorkPortal.model.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
     /**
