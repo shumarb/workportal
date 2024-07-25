ea2529401d8cf710d88905bbf6c05a6db2351456
S.Umar <shumarb@outlook.com>
2024-07-25 22:09:54 +0800

    Unit test for Internationalisation class

---
+++ src/test/java/com/example/WorkPortal/configuration/InternationalisationTest.java
@@ -0,0 +1,38 @@
+/**
+ * Unit tests for Internationalisation class.
+ */
+
+package com.example.WorkPortal.configuration;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class InternationalisationTest {
+
+    @Mock
+    Internationalisation internationalisation;
+
+    @BeforeEach
+    void setUp() {
+        // Arrange
+        internationalisation = new Internationalisation();
+    }
+
+    @Test
+    void test_localeChangeInterceptor() {
+        // Act
+        LocaleChangeInterceptor localeChangeInterceptor = internationalisation.localeChangeInterceptor();
+
+        // Assert
+        assertEquals(LocaleChangeInterceptor.class, localeChangeInterceptor.getClass());
+        assertEquals("language", localeChangeInterceptor.getParamName());
+    }
+
+}
