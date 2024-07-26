46efbf596bbce81883f04d0464afab09d89d0715
S.Umar <shumarb@outlook.com>
2024-07-26 09:11:12 +0800

    Updated unit tests

--- src/test/java/com/example/WorkPortal/controller/LoginControllerTest.java
+++ src/test/java/com/example/WorkPortal/controller/LoginControllerTest.java
@@ -3,20 +3,39 @@
  */
 package com.example.WorkPortal.controller;
 
+import com.example.WorkPortal.exceptions.InvalidUsernameOrPasswordException;
+import com.example.WorkPortal.model.Manager;
+import com.example.WorkPortal.model.Person;
+import com.example.WorkPortal.service.LoginService;
+import jakarta.servlet.http.HttpSession;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ui.Model;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class LoginControllerTest {
 
-    private String pageName;
+    String validName;
+    String validUsername;
+    String validEmail;
+    String validPassword;
+    String invalidUsername;
+    String invalidPassword;
+
+    @Mock
+    private HttpSession httpSession;
+
+    @Mock
+    private LoginService loginService;
 
     @Mock
     private Model model;
@@ -26,22 +45,40 @@ class LoginControllerTest {
 
     @BeforeEach
     public void setUp() {
-        this.pageName = loginController.showLoginPage();
+        MockitoAnnotations.openMocks(this);
+        validName = "Ali Hassan";
+        validUsername = "ali_hassan";
+        validEmail = "ali_hassan@yahoo.com";
+        validPassword = "TY43#";
+        invalidUsername = "ad";
+        invalidPassword = "12";
     }
 
     @Test
-    void test_retrievesLoginPage() {
-        assertEquals("login", pageName);
+    void test_showLoginPage() {
+        // Act
+        String viewName = this.loginController.showLoginPage();
+
+        // Assert
+        assertEquals("login", viewName);
     }
 
     @Test
-    void test_doesNotRetrieveIndexPage() {
-        assertNotEquals("index", pageName);
+    void test_doesNotShowIndexPage() {
+        // Act
+        String viewName = this.loginController.showLoginPage();
+
+        // Assert
+        assertNotEquals("index", viewName);
     }
 
     @Test
-    void test_doesNotRetrieveRegistrationPage() {
-        assertNotEquals("registration", pageName);
+    void test_doesNotShowRegistrationPage() {
+        // Act
+        String viewName = this.loginController.showLoginPage();
+
+        // Assert
+        assertNotEquals("registration", viewName);
     }
 
 }
