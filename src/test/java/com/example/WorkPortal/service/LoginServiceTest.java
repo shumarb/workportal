748a7f80d5910b0170f1b0855e2a73d2de7e962b
S.Umar <shumarb@outlook.com>
2024-09-13 11:32:58 +0800

    Specified assertions used in imports rather than importing all assertions

--- src/test/java/com/example/WorkPortal/service/LoginServiceTest.java
+++ src/test/java/com/example/WorkPortal/service/LoginServiceTest.java
@@ -18,7 +18,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.lenient;
 
 @ExtendWith(MockitoExtension.class)
